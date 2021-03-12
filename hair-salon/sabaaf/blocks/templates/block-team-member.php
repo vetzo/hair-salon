53f055191e08c946c8b93d9b2b303ffe2281f7ef
Vedran <vedranlutring@gmail.com>
2021-03-12 07:48:26 +0100

    fixes

--- hair-salon/sabaaf/blocks/templates/block-team-member.php
+++ hair-salon/sabaaf/blocks/templates/block-team-member.php
@@ -8,8 +8,10 @@
 		</div>
 		<div class="row justify-content-center">
 			<?php foreach( $team_members as $team_member ) : ?>
-				<div class="col-12 col-md-6 col-lg text-center">
-					<?php echo get_the_post_thumbnail( $team_member['id'], 'full', array( 'class' => 'mb-3' ) ); ?>
+				<div class="col-12 col-md-6 col-lg text-center team-member">
+					<div class="image">
+						<?php echo get_the_post_thumbnail( $team_member['id'], 'full', array( 'class' => 'mb-3' ) ); ?>
+					</div>
 					<h2 class="name"><?php echo get_the_title($team_member['id']); ?></h4>
 					<h4 class="position"><?php echo carbon_get_post_meta( $team_member['id'], 'position' ); ; ?></h4>
 				</div>
